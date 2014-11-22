2ca2ce6e2a62237e9d049988acfcf270e9010e36
Paul Howard <paul@paul-howard.net>
2014-11-22 15:33:23 -0800

    Some progress on design...

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 // can't do shit with the DOM 'til it's loaded, bitches
 document.addEventListener('DOMContentLoaded', function (){
     
+    var turnCounter = 0;
+
+    var claimSquare = function () {
+        alert('BOOM! BOOM!')
+        turnCounter += 1;
+        console.log(turnCounter);
+    }
+
     var anyBox = document.querySelectorAll(".box");
     console.log(anyBox);
 
     anyBox[0].addEventListener('touchstart', function() {
-        alert('BOOM!')
+        
     })
+
+    anyBox[0].addEventListener('click', claimSquare())
 
 });
 
