04fd0834941321273c4af2f166e0898c0e1a260a
Paul Howard <paul@paul-howard.net>
2014-11-22 23:08:37 -0800

    Changed a bunch of junk.

--- main.js
+++ main.js
@@ -1,22 +1,32 @@
+var turnCounter = 0;
+
+var claimSquare = function (squareToClaim) {
+    turnCounter += 1;
+    console.log(turnCounter);
+
+    if (turnCounter % 2 = 0) {
+        // claim for O
+    } else {
+        // claim for X
+    };
+}
+
+
+
 // can't do shit with the DOM 'til it's loaded, bitches
 document.addEventListener('DOMContentLoaded', function (){
     
-    var turnCounter = 0;
-
-    var claimSquare = function () {
-        alert('BOOM! BOOM!')
-        turnCounter += 1;
-        console.log(turnCounter);
-    }
-
     var anyBox = document.querySelectorAll(".box");
     console.log(anyBox);
+    console.log(anyBox.length);
 
     anyBox[0].addEventListener('touchstart', function() {
-        
-    })
+        claimSquare()
+    });
 
-    anyBox[0].addEventListener('click', claimSquare())
+    anyBox[0].addEventListener('click', function(){
+        claimSquare();
+    });
 
 });
 
